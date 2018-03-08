4bb87ab3a8637a5120deb24b5d19db06c33c0621
tfilosk@gmail.com <tfilosk@gmail.com>
2018-03-08 20:35:51 +0100

    Added enviromental variable PAFY_BACKEND, removed redundant pytest-capturelog since is included in core, changed version of python, changed caplog.text() to caplog.text according to documentation https://docs.pytest.org/en/latest/logging.html

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -98,7 +98,7 @@ def test_lookup_video_uri(caplog):
                             '/a title.C0DPdy98e4c')
 
     assert 'Need 11 character video id or the URL of the video.' \
-           not in caplog.text()
+           not in caplog.text
 
     assert track
 
