208f24c195937c47247cd9a82fef77da8b820571
natumbri <natumbri@gmail.com>
2019-04-26 05:10:16 +0000

    fix tests

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -10,3 +10,7 @@ def test_get_default_config():
 
     assert '[youtube]' in config
     assert 'enabled = true' in config
+    assert 'youtube_api_key = ' in config
+    assert 'threads_max = 16' in config
+    assert 'search_results = 15' in config
+    assert 'playlist_max_videos = 20' in config
