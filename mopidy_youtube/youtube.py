9d132595dca5cda1dd4e4a2c724f163bfe1212e3
natumbri <natumbri@gmail.com>
2019-11-21 07:57:47 +0000

    bs4 playlist

--- mopidy_youtube/youtube.py
+++ mopidy_youtube/youtube.py
@@ -754,7 +754,7 @@ class bs4API(scrAPI):
     # list playlist items
     #
     @classmethod
-    def WIP_run_list_playlistitems(cls, query):
+    def run_list_playlistitems(cls, query):
         items = []
 
         regex = (
@@ -766,38 +766,31 @@ class bs4API(scrAPI):
         result = cls.session.get(scrAPI.endpoint+'playlist', params=query)
         if result.status_code == 200:
             soup = BeautifulSoup(result.text, "html.parser")
-            videos = soup.find_all("div", {'class': 'yt-lockup-video'})
+            videos = soup.find_all("tr", {'class': 'pl-video'})
             for video in videos:
-                duration = cls.format_duration(re.match(regex, video.find(class_ = "video-time").text))
+                duration = cls.format_duration(re.match(regex, video.find(class_ = "timestamp").text))
                 item = {
                         'id': {
                             'kind': 'youtube#video',
-                            'videoId': video['data-context-item-id']
+                            'videoId': video['data-video-id']
                         },
                         'contentDetails': {
                             'duration': 'PT'+duration,
                         },
                         'snippet': {
-                            'title': video.find(class_ = "yt-lockup-title").next.text,
+                            'title': video['data-title'],
                             # TODO: full support for thumbnails
                             'thumbnails': {
                                 'default': {
-                                    'url': "https://i.ytimg.com/vi/"+video['data-context-item-id']+"/default.jpg",
+                                    'url': "https://i.ytimg.com/vi/"+video['data-video-id']+"/default.jpg",
                                     'width': 120,
                                     'height': 90,
                                 },
                             },
-                            'channelTitle': video.find(class_ = "yt-lockup-byline").text,
-                            # 'uploadDate': video.find(class_ = "yt-lockup-meta-info").find_all("li")[0].text,
-                            # 'views': video.find(class_ = "yt-lockup-meta-info").find_all("li")[1].text,
-                            # 'url': 'https://www.youtube.com'+video.find(class_ = "yt-lockup-title").next['href'] 
+                            'channelTitle': video.find(class_ = "pl-video-owner").find("a").text,
                         },
                     }
 
-                # if video.find(class_ = "yt-lockup-description") is not None:
-                #   item['snippet']['description'] = video.find(class_ = "yt-lockup-description").text or "NA"
-                # else:
-                #   item['snippet']['description'] = "NA"
                 items.append(item)
         return items
 
