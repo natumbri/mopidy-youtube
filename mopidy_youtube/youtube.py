ae41ad3de646d79313f6c86702642edf467a82ba
Nik Tumbri <natumbri@gmail.com>
2021-08-02 10:34:06 +1000

    black and flake8

--- mopidy_youtube/youtube.py
+++ mopidy_youtube/youtube.py
@@ -179,11 +179,11 @@ class Entry:
                 )
                 if m:
                     val = (
-                            int(m.group("weeks") or 0) * 604800
-                            + int(m.group("days") or 0) * 86400
-                            + int(m.group("hours") or 0) * 3600
-                            + int(m.group("minutes") or 0) * 60
-                            + int(m.group("seconds") or 0)
+                        int(m.group("weeks") or 0) * 604800
+                        + int(m.group("days") or 0) * 86400
+                        + int(m.group("hours") or 0) * 3600
+                        + int(m.group("minutes") or 0) * 60
+                        + int(m.group("seconds") or 0)
                     )
                 else:
                     val = 0
@@ -230,7 +230,7 @@ class Video(Entry):
             executor.map(
                 job,
                 [
-                    listOfVideos[i: i + 50]
+                    listOfVideos[i : i + 50]
                     for i in range(0, len(listOfVideos), 50)
                 ],
             )
@@ -348,7 +348,7 @@ class Playlist(Entry):
             executor.map(
                 job,
                 [
-                    listOfPlaylists[i: i + batch]
+                    listOfPlaylists[i : i + batch]
                     for i in range(0, len(listOfPlaylists), batch)
                 ],
             )
@@ -367,8 +367,8 @@ class Playlist(Entry):
             data = {"items": []}
             page = ""
             while (
-                    page is not None
-                    and len(data["items"]) < self.playlist_max_videos
+                page is not None
+                and len(data["items"]) < self.playlist_max_videos
             ):
                 try:
                     max_results = min(
@@ -390,7 +390,7 @@ class Playlist(Entry):
 
                 data["items"].extend(result["items"])
 
-            del data["items"][int(self.playlist_max_videos):]
+            del data["items"][int(self.playlist_max_videos) :]
 
             myvideos = []
 
@@ -494,13 +494,13 @@ class Client:
 
     @classmethod
     def _create_session(
-            cls,
-            proxy,
-            headers,
-            retries=10,
-            backoff_factor=0.3,
-            status_forcelist=(500, 502, 504),
-            session=None,
+        cls,
+        proxy,
+        headers,
+        retries=10,
+        backoff_factor=0.3,
+        status_forcelist=(500, 502, 504),
+        session=None,
     ):
         cls.session = session or requests.Session()
         retry = Retry(
