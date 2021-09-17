ef021c1f69a5b3f2ea2aa29f3f91d60e90ce13b9
Nik Tumbri <natumbri@gmail.com>
2021-09-17 23:06:50 +1000

    add tests for youtube.py

---
+++ tests/test_youtube.py
@@ -0,0 +1,121 @@
+import pytest
+
+from mopidy_youtube import youtube
+
+from tests import apis, my_vcr
+from tests.test_api import setup_entry_api
+
+
+@pytest.mark.parametrize("api", apis)
+def test_youtube_search(api, config, headers):
+
+    with my_vcr.use_cassette(
+        f"tests/fixtures/{api['name']}/youtube_search.yaml",
+        filter_query_parameters=["key"],
+    ):
+        setup_entry_api(api, config, headers)
+        search_results = youtube.Entry.search("test")
+
+        assert isinstance(search_results, list)
+
+
+@pytest.mark.parametrize("api", apis)
+def test_youtube_get_video(api, config, headers):
+
+    with my_vcr.use_cassette(
+        f"tests/fixtures/{api['name']}/youtube_get_video.yaml",
+        filter_query_parameters=["key"],
+    ):
+        setup_entry_api(api, config, headers)
+
+        video = youtube.Video.get("e1YqueG2gtQ")
+
+        assert video.length.get()
+        assert video.related_videos.get()
+        assert video.thumbnails.get()
+        assert video.is_video is True
+        assert video.channel.get()
+
+        # get again, should fetch from cache, _length should be ready
+        video2 = youtube.Video.get("e1YqueG2gtQ")
+
+        assert video2 is video
+        assert video2._length
+
+
+@pytest.mark.parametrize("api", apis)
+def test_audio_url(api, config, headers, youtube_dl_mock_with_video):
+
+    setup_entry_api(api, config, headers)
+    youtube.Video.proxy = None
+    video = youtube.Video.get("e1YqueG2gtQ")
+
+    assert video.audio_url.get()
+
+
+@pytest.mark.parametrize("api", apis)
+def test_audio_url_fail(api, config, headers, youtube_dl_mock):
+
+    setup_entry_api(api, config, headers)
+    youtube.Video.proxy = None
+
+    youtube_dl_mock.YoutubeDL.side_effect = Exception("Removed")
+
+    video = youtube.Video.get("unknown")
+
+    assert not video.audio_url.get()
+
+
+@pytest.mark.parametrize("api", apis)
+def test_youtube_get_playlist(api, config, headers):
+
+    with my_vcr.use_cassette(
+        f"tests/fixtures/{api['name']}/youtube_get_playlist.yaml",
+        filter_query_parameters=["key"],
+    ):
+        setup_entry_api(api, config, headers)
+        youtube.Playlist.playlist_max_videos = config["youtube"]["playlist_max_videos"]
+        pl = youtube.Playlist.get("PLJD13y84Bd032qVrq7CHBLEfKZZtp-u1j")
+
+        assert pl.video_count.get()
+        assert pl.thumbnails.get()
+        assert pl.is_video is False
+        assert pl.channel.get()
+
+        assert len(pl.videos.get()) == 20
+        assert pl.videos.get()[0].title.get()
+
+        # Playlist.videos starts loading video info in the background
+        video = pl.videos.get()[0]  # don't know what videos[0] will be
+        assert video._title
+        assert video._channel
+        assert video._length
+
+        # don't know what the length will be because we don't know what videos[0] is
+        # not sure where the order is non-deterministic
+        # assert video.length.get() == 277
+
+        # don't know what the length will be because we don't know what videos[0] is
+        # not sure where the order is non-deterministic
+        assert isinstance(video.title.get(), str)
+        assert isinstance(video.channel.get(), str)
+        assert isinstance(video.length.get(), int)
+
+        pl2 = youtube.Playlist.get("PLJD13y84Bd032qVrq7CHBLEfKZZtp-u1j")
+
+        assert pl2 is pl  # fetch from cache
+        assert pl._videos  # should be ready
+
+
+@pytest.mark.parametrize("api", apis)
+def test_youtube_channel_playlists(api, config, headers):
+
+    with my_vcr.use_cassette(
+        f"tests/fixtures/{api['name']}/youtube_channel_playlists.yaml",
+        filter_query_parameters=["key"],
+    ):
+        setup_entry_api(api, config, headers)
+        channel_playlists = youtube.Channel.playlists("UCZtGOj7FTHPd2txgnbJS2kQ")
+
+        assert isinstance(channel_playlists, list)
+        assert len(channel_playlists) > 0
